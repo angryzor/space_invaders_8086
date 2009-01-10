3f6abb4a40bd7a93928734ab0a40a739d7314b35
angryzor <angryzor@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-01-10 21:16:18 +0000

    Oops file vergete
    git-svn-id: https://subversion.assembla.com/svn/space_invaders_8086@73 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ themfire.asm
@@ -0,0 +1,72 @@
+random2 proc near USES DX
+a=16333
+b=25887
+    mov ax,randomnumber
+    ;times a
+    mov dx,a
+    mul dx
+    ;add b
+    add ax,b ;no need for adc, bits will be lost anyway
+    ;mask 15 first bits to perform mod 2^15
+    and ax,7FFFh
+    mov randomnumber,ax
+    ret
+random2 endp
+
+theyTryToFire PROC NEAR USES AX BX
+	cmp byte ptr bTheirBulletExists, 1
+	jz noFire
+	
+	call random2
+;	mov bl, cNumMonsters
+;	div bl
+	
+;	mov bl, ah
+;	xor bh, bh
+	and ax, 1111100b
+	mov bx, ax
+	
+	add bx, offset wwEnemyPositions
+
+	mov ax, [bx]
+	add ax, 8
+	mov wTheirBulletX, ax
+	
+	mov ax, [bx+2]
+	add ax, 14
+	mov wTheirBulletY, ax
+	
+	mov bTheirBulletExists, 1
+noFire:
+	ret
+theyTryToFire ENDP
+
+updateTheirBulletPosition MACRO
+	cmp byte ptr bTheirBulletExists, 0
+	jz updateTheirBulletPositionEnd
+	mov ax, wTheirBulletY
+	add ax, 2
+	cmp ax, cScrHeight-5
+	jb theirBulletNoDestroy
+	mov bTheirBulletExists, 0
+theirBulletNoDestroy:
+	mov wTheirBulletY, ax
+updateTheirBulletPositionEnd:
+ENDM
+
+checkShipHit PROC NEAR USES SI BX DI
+	mov si, offset bSpaceship
+	mov di, offset shipX
+	mov bx, offset wTheirBulletX
+	
+	call collCheckHit
+	jz exitproc
+	
+	mov bTheirBulletExists, 0
+	dec bLives
+	jnz exitproc
+	
+	mov bGameOver, 1
+exitproc:
+	ret
+checkShipHit ENDP
