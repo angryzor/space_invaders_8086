063362c8e2906c198376a263f460dd63ba7c8fbc
Banni <Banni@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-01-12 20:16:34 +0000

    git-svn-id: https://subversion.assembla.com/svn/space_invaders_8086@90 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- sprites.asm
+++ sprites.asm
@@ -17,6 +17,38 @@ db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h
 db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
 db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
 db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+
+bMonster2 dw	16, 14
+db				0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh,0ffh,0ffh
+db				0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh
+db				0ffh,0b5h,0b5h,0b5h,0b5h,0b3h,0b3h,0b5h,0b5h,0b3h,0b3h,0b5h,0b5h,0b5h,0b5h,0ffh
+db				0b5h,0b5h,0b5h,0b5h,0b5h,0b8h,0b3h,0b5h,0b5h,0b8h,0b3h,0b5h,0b5h,0b5h,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+
+bMonster3 dw	16, 14
+db				0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh,0ffh,0ffh
+db				0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh
+db				0ffh,0b5h,0b5h,0b5h,0b5h,0b3h,0b3h,0b5h,0b5h,0b3h,0b3h,0b5h,0b5h,0b5h,0b5h,0ffh
+db				0b5h,0b5h,0b5h,0b5h,0b5h,0b8h,0b3h,0b5h,0b5h,0b8h,0b3h,0b5h,0b5h,0b5h,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0ffh,0ffh,0ffh,0ffh,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
+db				0ffh,0ffh,0ffh,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0b5h,0b5h,0ffh,0ffh,0ffh,0ffh,0ffh
 				
 bBullet dw 		3, 5
 db				0ffh,0b5h,0ffh
