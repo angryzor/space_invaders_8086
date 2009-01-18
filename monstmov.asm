b7510b3560070a7a4a211668ebeeeb5d8777a239
BlindSight <BlindSight@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-01-18 21:52:06 +0000

    git-svn-id: https://subversion.assembla.com/svn/space_invaders_8086@115 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- monstmov.asm
+++ monstmov.asm
@@ -10,24 +10,24 @@ updateMonsterPositions PROC NEAR USES SI dx cx ax
 	mov es, ax
 	mov SI, offset wwEnemyPositions
 	mov dx, bIncValue
-	mov cx, cNumMonsters                     ; nr of monsters
-moveAll:
-	mov ax, [SI]
-	add ax,dx
-	mov [SI], ax
-	add SI, 4 		;we use words and y doesn't need to be adjusted
+	mov cx, cNumMonsters					;nr of monsters
+moveAll:									;move all monster bIncValue pixels ( bIncValue can be negative or positive) 
+	mov ax, [SI]							;x-coordinate of monster
+	add ax,dx								;add bIncValue to x-coordinate
+	mov [SI], ax							;change value in wwEnemyPositions
+	add SI, 4 								;we use words and y doesn't need to be adjusted
 	loop moveAll
 movement:
-	mov SI, offset wwEnemyPositions
+	mov SI, offset wwEnemyPositions			;back to the start of wwEnemyPositions
 	mov ax, [SI]
-	cmp ax, cMaxPos
+	cmp ax, cMaxPos							;x-coordinate of the first monster must be >= 15 and <= 75			(>= because they will change the next time function is called)
 	jge endScreen
 	cmp ax, cMinPos
 	jle endScreen
 	jmp positionsUpdated
 	
 	
-endScreen:	
+endScreen:									; change sign bIncValue so monsters will move to opposite direction
 	neg dx
 	mov bIncValue, dx
 positionsUpdated:
